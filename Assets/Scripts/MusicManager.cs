3d1cc0547e1e07d3f8e5c809cc005ef9933061e9
Mayank Raj <AgrMayank@outlook.com>
2020-05-25 12:44:29 +0530

    Initial commit

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+	public AudioClip[] levelMusicChangeArray;
+
+	private AudioSource audioSource;
+
+	private void Awake ()
+	{
+		DontDestroyOnLoad (gameObject);
+		Debug.Log ("Don't destroy on load " + name);
+	}
+
+	private void Start ()
+	{
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	private void OnLevelWasLoaded (int level)
+	{
+		AudioClip thisLevelMusic = levelMusicChangeArray [level];
+		Debug.Log ("Playing Clip : " + thisLevelMusic);
+
+		//If there is some music attached
+		if (thisLevelMusic)
+		{
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true;
+			audioSource.Play ();
+		}
+	}
+
+	public void ChangeVolume (float volume)
+	{
+		audioSource.volume = volume;
+	}
+}
---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+	public Slider VolumeSlider;
+	public Slider DifficultySlider;
+	public LevelManager levelManager;
+
+	private MusicManager musicManager;
+
+	// Start is called before the first frame update
+	void Start ()
+    {
+		musicManager = GameObject.FindObjectOfType<MusicManager> ();
+
+		VolumeSlider.value = PlayerPrefsManager.GetMasterVolume ();
+		DifficultySlider.value = PlayerPrefsManager.GetDifficulty ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		musicManager.ChangeVolume (VolumeSlider.value);
+    }
+
+	public void SaveAndExit ()
+	{
+		PlayerPrefsManager.SetMasterVolume (VolumeSlider.value);
+		PlayerPrefsManager.SetDifficulty (DifficultySlider.value);
+
+		levelManager.LoadLevel ("01a Start");
+	}
+
+	public void SetDefaults ()
+	{
+		VolumeSlider.value = 0.8f;
+		DifficultySlider.value = 2f;
+	}
+}
